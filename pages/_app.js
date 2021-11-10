c0c8017a621f73b042de1159f149f4460c245f15
omar mokhtar <al_fagomy22@yahoo.com>
2021-11-10 14:41:26 +0300

    social

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fab,fas } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import '../styles/globals.scss'
 import Layouts from '../components/Layouts'
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.css';
 import Image from 'next/image'
 import photo from '../public/images/omar.png'
+import envelope from '../public/images/envelope.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
@@ -17,20 +19,50 @@ export default function Home() {
           <div className="container">
   
           <div className="index__details">
-              <div className="row">
-                <div className="col col-lg-3">
-                <FontAwesomeIcon icon={['fab', 'twitter']} />
+              <div className="row align-items-center">
+                <div className="col col-lg-5">
+             
                 <Image
                 alt="MyImage"
                 src={photo}
                       />
                 </div>
-                <div className="col col-lg-8">
+                <div className="col col-lg-7">
               <div className="title">
-OMAR MOKHTAR
+             <div className="myname d-flex align-items-center">
+             OMAR <span className="m-2 cusomspan  ">MOKHTAR</span>
+               </div>
+
+                <span className="">
+                  UI/UX Designer
+                </span>
 
               </div>
-                 <button className="btn mt-3">Download</button>
+
+              <div className="mt-3 d-flex align-items-center discraption">
+
+                <p>
+                I am an experienced UI/User Experience Designer.
+Over the past 10 years, I've designed digital experiences (mobile, web, dashboards, and more) across a variety of organizations, minstiies, and universities.
+                </p>
+
+                  <div className="socialIcon">
+                  <a href="https://www.behance.net/Omar_Mokhtar" title="behance">  <FontAwesomeIcon icon={['fab', 'behance']} /></a>
+                  <a href="https://github.com/omarmok" title="github">  <FontAwesomeIcon icon={['fab', 'github']} /></a>
+                  <a href="https://www.linkedin.com/in/omarmokhtar22/" title="linkedin">  <FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+                  <a href="mailto:sal_fagomy22@hotmail.com" title="envelope">      <Image alt="MyImage" src={envelope} class="envelopeimage" /></a>
+                  <a href="https://twitter.com/al_fagomy22" title="twitter">  <FontAwesomeIcon icon={['fab', 'twitter']} /></a>
+                    </div>
+
+
+                </div>
+               
+              <div className="d-flex">
+                    <button className="btn mt-3 mx-2">DOWNLOAD PORTFOLIO</button>
+                    <button className="btn outlinebtn mt-3">Projects</button>
+                    
+                </div>
+
                 </div>
                 </div>
       
