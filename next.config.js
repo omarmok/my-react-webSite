a0a4b05d4c87e5c1892d7abb4aabb32789de4155
omar mokhtar <al_fagomy22@yahoo.com>
2021-11-21 11:29:21 +0300

    css

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     loader: 'imgix',
-    path: ['http://omarmokhtar.com/forjeson/']
+    path: ['https://omarmokhtar.com/forjeson/']
   },
   
 }
