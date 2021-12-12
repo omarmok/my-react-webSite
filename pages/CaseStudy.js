ed95751b974fad2b695e151b43f6e0a651e0e75a
omar mokhtar <al_fagomy22@yahoo.com>
2021-12-12 11:17:19 +0300

    CompetitiveAnalysis

--- pages/CaseStudy.js
+++ pages/CaseStudy.js
@@ -4,12 +4,19 @@ import Image from 'next/image'
 import case1 from '../public/images/case1.png'
 import walidInfo from '../public/images/walidInfo.png'
 import color from '../public/images/color.png'
+import CompetitiveAnalysis from '../public/images/CompetitiveAnalysis.png'
+import EMP from '../public/images/EMP.png'
+import define from '../public/images/define.png'
+import ideate from '../public/images/ideate.png'
+import proto from '../public/images/proto.png'
+import test from '../public/images/test.png'
+import prototype from '../public/images/prototype.png'
 import screenshoot from '../public/images/screenshoot.png'
 import Loader from '../components/Loader'
 const CaseStudy = () => {
     return(
 
-
+      
         
         <div >
          
@@ -27,25 +34,106 @@ const CaseStudy = () => {
             </div>
           <div className="casestudyContainer" data-aos="fade-up" data-aos-duration="2000">
           <Image  alt="MyImage"  src={case1}  layout="responsive" />
+          <br/>
+          <h3 className="fw-bold">My Role</h3>
+
+        <p>
+        I was the UX Designer UI Designer UI Developer in a team of 6 and we completed this project over 10 weeks.
+          <br/>
+          I was in competitives analysis and user research.
+          <br/>
+          I was also responsible for coming up with ideas for designing the layout, and for improving the overall user experience using sketching, wireframing and Prototyping. 
+          <br/>
+          Moreover, I was involved with the development team, as we used HTML and Javascript to build portal . 
+
+          <br/>
+          Working closely with analysist ,developers and QA testers ensuring that the product is developed according to experience and design specifications.
+
+                    </p>
+                    <br/>
           {/* <Image  alt="MyImage"  src="../public/images/case1.png" width={900} height={450} layout='responsive' objectFit='contain' /> */}
 
+          <h3 className="fw-bold ">Process</h3>
+
+          <div className='row'>
+
+            <div className='col-lg-4 col-sm-12'>
+              <div className='processcat p-3 bg-white'>
+
+              <Image  alt="MyImage"  src={EMP}  />
+
+                    <p>
+                    Communicate with users in order to understand their problems and needs in general through workshops and direct meetings and through various call centers
+
+                    </p>
+              </div>
+            </div>
+
+
+            <div className='col-lg-4 col-sm-12'>
+              <div className='processcat p-3 bg-white'>
+
+              <Image  alt="MyImage"  src={define}   />
+
+                    <p>
+                    After collecting information from students, studying competitors and studying the current situation, we determine what is required to start the next stage                    </p>
+              </div>
+            </div>
+
+
+            <div className='col-lg-4 col-sm-12'>
+              <div className='processcat p-3 bg-white'>
+
+              <Image  alt="MyImage"  src={ideate}   />
+
+                    <p>
+                    Gather all the previous information brainstorming meetings with teammates to come up with a set of ideas that help us solve this problem
+                    </p>
+              </div>
+            </div>
+
+
+            <div className='col-lg-4 col-sm-12'>
+              <div className='processcat p-3 bg-white'>
+
+              <Image  alt="MyImage"  src={proto}   />
+
+                    <p>
+                    Selecting the best ideas from the thinking stage and providing prototypes for some ideas To clarify the idea of the solution that we will do
+                    </p>
+              </div>
+            </div>
+
+
+            <div className='col-lg-4 col-sm-12'>
+              <div className='processcat p-3 bg-white'>
+
+              <Image  alt="MyImage"  src={test}   />
+
+                    <p>
+                    Present prototypes to a group of education students on their opinions about it and record their comments to have final result to move on                    </p>
+              </div>
+            </div>
+
+
+
+
+
+
+          </div>
+
+            <div className='Competitive'>
+              
+
+            <h3 className="fw-bold ">Competitive Analysis</h3>
+
+            <p>Competitive analysis involves identifying your direct and indirect competitors using research to reveal their strengths and weaknesses in relation to your own.</p>
+                <div className='kpishint'>
+                *all kpis based on one page
+                </div>
+                <Image className='mt-1'  alt="MyImage"  src={CompetitiveAnalysis}   />
+            </div>
         
-              <h3 className="fw-bold">My Role</h3>
-
-                <p>
-                I was the UX Designer UI Designer UI Developer in a team of 6 and we completed this project over 10 weeks.
-                  <br/>
-                  I was in competitives analysis and user research.
-                  <br/>
-                  I was also responsible for coming up with ideas for designing the layout, and for improving the overall user experience using sketching, wireframing and Prototyping. 
-                  <br/>
-                  Moreover, I was involved with the development team, as we used HTML and Javascript to build portal . 
-
-                  <br/>
-                  Working closely with analysist ,developers and QA testers ensuring that the product is developed according to experience and design specifications.
-
-                            </p>
-                      
 
                           <div className="Persona mt-5">
                               <h3 className="fw-bold">Persona</h3>
@@ -142,6 +230,7 @@ const CaseStudy = () => {
                   </div>
 
                   </div>
+                  <Image  alt="MyImage"  src={prototype}  layout="responsive" />
 
                 {/* <Image  alt="MyImage"  src="../public/images/Persona.png" width={900} height={350} layout='responsive' objectFit='contain' /> */}
                 <Image  alt="MyImage"  src={color}  width={900} height={500} layout="responsive" />
