d9e58710562a90f134fec10d05db645813f40aa1
omar mokhtar <al_fagomy22@yahoo.com>
2022-02-13 08:57:20 +0300

    hot-remove-component

--- components/hotjar.js
+++
@@ -1,9 +0,0 @@
-import { hotjar } from 'react-hotjar'
-import { useEffect } from 'react'
-function hotjar ({ Component, pageProps }) {
-  useEffect(() => {
-    hotjar.initialize(1978942, 1)
-  }, [])
-return <Component {...pageProps} />
-}
-export default hotjar
