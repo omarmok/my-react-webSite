c843dc2ecefa0c3f158157127f7bd043a4037806
Omar Mokhtar <al_fagomy22@hotmail.com>
2023-07-13 09:45:37 +0300

    change footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,10 @@
 const Footer = () => {
     return(
         <div className="footer text-center">
-           <a href="http://omarmokhtar.com/" target="_self">&copy; OMAR MOKHTAR</a>
+     
+           <a href="http://omarmokhtar.com/" target="_self">      Made with
+           <span className="text-danger ms-1 me-1">	&#10084;</span>
+            from scratch by &copy; OMAR MOKHTAR</a>
         </div>
 
     )
