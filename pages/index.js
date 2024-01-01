3a844757cae8bab96601b121b432f57f1ed0d79e
Omar Mokhtar <al_fagomy22@hotmail.com>
2024-01-01 07:43:59 +0300

    some enhanc

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.css';
 import Image from 'next/image';
 import photo from '../public/images/omar.png';
-import googleimage from '../public/images/google.png';
+
 import { FaTwitter, FaBehance, FaGithub, FaEnvelope, FaLinkedin, FaDownload } from 'react-icons/fa';
 import Loader from '../components/Loader';
 
@@ -80,7 +80,10 @@ export default function Home() {
                     className='googlecertificated'
                
                   >
-                    <Image alt="Google Certified" src={googleimage} />
+                    {/* <Image alt="Google Certified" src={googleimage} /> */}
+
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/><path d="M1 1h22v22H1z" fill="none"/></svg>
+
                     Google Certified
                   </a>
                 </div>
@@ -88,22 +91,20 @@ export default function Home() {
 
                    <div className='d-flex align-items-center flex-column'  >
 
-                   <p data-bs-toggle="tooltip" data-bs-placement="top" title="
-                   مع أكثر من 14 عامًا من الخبرة المخصصة كمصمم واجهة المستخدم/تجربة المستخدم، فقد تخصصت في صياغة تجارب رقمية مقنعة وواجهات متنقلة ولوحات تحكم ويب ديناميكية. طوال مسيرتي المهنية، كان لي شرف المساهمة بخبرتي في قطاعات متنوعة بما في ذلك المنظمات الشهيرة والوزارات الحكومية والمؤسسات الأكاديمية المرموقة.
- ">
-                    With over 14 years of dedicated experience as a UI/User Experience Designer, I&apos;ve specialized in crafting compelling digital experiences, mobile interfaces, and dynamic web dashboards. Throughout my career, I&apos;ve had the privilege to contribute my expertise across diverse sectors including renowned organizations, government ministries, and esteemed academic institutions.
+                   <p data-bs-toggle="tooltip" data-bs-placement="top" title="مع أكثر من 14 عامًا من الخبرة كمصمم UI/UX، فأنا متخصص في التجارب الرقمية وواجهات الأجهزة المحمولة ولوحات معلومات الويب .">
+                  
+                    With over 14 years of experience as a UI/UX designer, I specialize in digital experiences, mobile interfaces, and dynamic web dashboards.
+                 
                   </p>
-                  <p data-bs-toggle="tooltip" data-bs-placement="top" title="
-ويمتد نهجي إلى ما هو أبعد من مجرد التصور - فأنا أتولى قيادة مبادرات إعادة التصميم الشاملة، وتحويل تجارب المستخدم من خلال ترجمة الأفكار بسلاسة إلى حلول ملموسة. بدءًا من التفكير الأولي وحتى التنفيذ النهائي، تتضمن عمليتي رسمًا تخطيطيًا دقيقًا وإطارًا سلكيًا دقيقًا وصياغة نماذج أولية عالية الدقة. ">
-                    My approach extends beyond conceptualization—I take the helm in leading comprehensive redesign initiatives, transforming user experiences by seamlessly translating ideas into tangible solutions. From initial ideation to the final execution, my process involves meticulous sketching, precise wireframing, and crafting high-fidelity prototypes.
-                  </p>
-                  <p data-bs-toggle="tooltip" data-bs-placement="top" title="
-التعاون هو جوهر سير العمل الخاص بي. أشارك بنشاط مع المحللين والمطورين ومختبري ضمان الجودة، مما يعزز العلاقة التكافلية التي تضمن الانتقال السلس من مخطط التصميم إلى المنتجات الوظيفية سهلة الاستخدام والتي تتماشى بدقة مع مواصفات المشروع.
-">
+                  <p data-bs-toggle="tooltip" data-bs-placement="top" title="طوال مسيرتي المهنية، عملت في قطاعات تشمل منظمات مشهورة ووزارات حكومية ومؤسسات أكاديمية مرموقة. ">
+Throughout my career, I have worked in sectors including renowned organizations, government ministries, and prestigious academic institutions.                  </p>
+                  <p data-bs-toggle="tooltip" data-bs-placement="top" title="أتولى قيادة مبادرات إعادة التصميم الشاملة، وتحويل تجارب المستخدم من خلال ترجمة الأفكار بسلاسة إلى حلول ملموسة، بدءًا من التفكير الأولي وحتى التنفيذ النهائي، وتتضمن عمليتي رسمًا دقيقًا، وإطارًا شبكيًا دقيقًا، وصياغة نماذج أولية عالية الدقة.">
   
-                    Collaboration is at the heart of my workflow. I actively engage with analysts, developers, and QA testers, fostering a symbiotic relationship that ensures a smooth transition from design blueprint to user-friendly, functional products aligned precisely with project specifications.
-                  </p>
+  I take the helm in leading comprehensive redesign initiatives, transforming user experiences by seamlessly translating ideas into tangible solutions From initial ideation to the final execution, my process involves meticulous sketching, precise wireframing, and crafting high-fidelity prototypes.                  </p>
 
+<p data-bs-toggle="tooltip" data-bs-placement="top" title='أعمل بشكل وثيق مع المحللين والمطورين ومختبري ضمان الجودة للتأكد من أن تصميماتنا تصبح منتجات سهلة الاستخدام وتتوافق تمامًا مع أهداف المشروع.'>
+I work closely with analysts, developers, and QA testers to ensure our designs smoothly become user-friendly products that match project goals perfectly.
+</p>
                    </div>
                
 
