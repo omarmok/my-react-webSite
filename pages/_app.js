625af98ba459b60c1ff041c6bddda28896e7b2c7
Omar Mokhtar <al_fagomy22@hotmail.com>
2024-09-25 15:45:35 +0300

    fix  font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import { useEffect } from "react";
 import { hotjar } from 'react-hotjar';
-
-
 import Head from 'next/head';
 import AOS from "aos";
 import axios from "axios";
@@ -9,7 +7,8 @@ import "aos/dist/aos.css";
 import 'bootstrap/dist/css/bootstrap.css'
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
-import { AnimatePresence } from "framer-motion";
+// import { AnimatePresence } from "framer-motion";
+
 import '../styles/globals.scss';
 import Layouts from '../components/Layouts';
 config.autoAddCss = false;
@@ -42,9 +41,10 @@ function MyApp({ Component, pageProps }) {
               <title>Omar Mokhtar</title>
               <meta name="description" content="UI/UX Designer" />
               <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
               <link rel="icon" href="/favicon.ico" />
-           
+              <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+              <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
             </Head>
             <Layouts>
               
