ec197998b5326850031a92c942b0d29a9ddccafa
Omar Mokhtar <al_fagomy22@hotmail.com>
2025-08-25 08:21:36 +0300

    fix font

--- pages/_app.js
+++ pages/_app.js
@@ -61,7 +61,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@100;200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />    
             </Head>
