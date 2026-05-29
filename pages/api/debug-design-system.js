a323194d474ac209200c9bfed0ef5e94489f807d
Omar Mokhtar <al_fagomy22@hotmail.com>
2026-05-29 13:51:14 +0300

    chore: remove temporary design system env debug endpoint

--- pages/api/debug-design-system.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    hasPassword: Boolean(process.env.DESIGN_SYSTEM_PASSWORD),
-    passwordLength: process.env.DESIGN_SYSTEM_PASSWORD?.length || 0,
-    nodeEnv: process.env.NODE_ENV,
-    vercelEnv: process.env.VERCEL_ENV,
-  });
-}
